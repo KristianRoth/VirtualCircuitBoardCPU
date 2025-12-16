efa41d9c8d8dd39c855480112370354989ebfb76
KristianRoth <kristian.roth95@gmail.com>
2025-12-16 21:43:38 +0200

    Removed rust parser

--- parser/target/debug/deps/parser-f51cc4b3a9e9b4cd.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/Kristian/Documents/VirtualCircuitBoard/projects/parser/target/debug/deps/parser-f51cc4b3a9e9b4cd.d: src/main.rs
-
-/mnt/c/Users/Kristian/Documents/VirtualCircuitBoard/projects/parser/target/debug/deps/parser-f51cc4b3a9e9b4cd: src/main.rs
-
-src/main.rs:
--- parser/target/debug/parser.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/Kristian/Documents/VirtualCircuitBoard/projects/parser/target/debug/parser: /mnt/c/Users/Kristian/Documents/VirtualCircuitBoard/projects/parser/src/main.rs
